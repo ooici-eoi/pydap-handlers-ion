5af26e99318023b93ced7b1bf1b202b0bd7b69e5
Christopher Mueller <cmueller@asascience.com>
2011-12-09 10:11:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+---
+**Ocean Observatories Initiative Cyberinfrastructure** 
+**Integrated Observatory Network (ION)** 
+
+pydap-handler-ion - Pydap handler for serving data from OOI-CI ION
+
+(C) UC Regents, 2010-2011
+
+---
+
+# Description
+*some really excellent description*
+
+**References**  
+*references to any architecture, design, or external pages*
+
+#Prerequisites
+
+This assumes basic development environment setup (git, directory structure). Please follow the
+"New Developers Tutorial" for basic steps.
+
+Pyon: The main dependency of this repository is the pyon Capability Container. Follow the listed
+steps to install the minimal needed dependencies to run pyon on a Mac. For more details and Linux
+install instructions, check out the pyon README: https://github.com/ooici/pyon/blob/master/README
+
+
+
+Install the following if not yet present:
+
+- git 1.7.7: Download the Mac or Linux installer and run it
+
+- OS Packages and package management:
+For Mac, use homebrew
+    > /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+- python 2.7
+*see below for installation*
+
+* couchdb 1.1.0 (optional if memory mockdb is used)
+*see below for installation*
+
+- rabbitmq 2.6.1 or later (recommended, but can use rabbitmq on amoeba)
+    *Download generic Linux version and unpack into a suitable directory.
+    **Note:** May need erl (Erlang) and some dependencies installed before*
+
+- **Install** libevent, libyaml, zeromq, couchdb, python, and rabbitmq with Homebrew
+    > brew install libevent libyaml zeromq couchdb python rabbitmq
+
+    You can even reinstall git using brew to clean up your /usr/local directory
+    Be sure to read the pyon README for platform specific guidance to installing
+    dependent libraries and packages.
+    Linux: Note that many installs have much older versions installed by default.
+    You will need to upgrade couchdb to at least 1.1.0.
+
+Python packages and environment management:
+
+- pip
+    > easy_install pip
+
+- virtualenv and virtualenvwrapper modules for your python 2.7 installation
+    > easy_install --upgrade virtualenv
+    > easy_install --upgrade virtualenvwrapper
+    Note: This may require Mac's XCode (use XCode 3.3 free version)
+
+- Setup a virtualenv to run COI-services (use any name you like):
+    > mkvirtualenv --no-site-packages --python=python2.7 coi
+    Note: Do not use the pyon virtualenv if you are a pyon developer
+
+###Compiled Library Dependencies
+- hdf5 library 
+
+     > brew install hdf5
+
+- netcdf
+
+    > brew install netcdf
+
+#Source
+
+Obtain the eoi-agents project by running:  
+
+    git clone git@github.com:ooici-eoi/pydap-handler-ion.git
+    cd pydap-handler-ion
+
---
+++ pydap/handlers/ion/__init__.py
@@ -0,0 +1,91 @@
+import re, os
+from configobj import ConfigObj
+from pydap.model import *
+from pydap.handlers.lib import BaseHandler
+from pydap.exceptions import OpenFileError
+import numpy
+from netCDF4 import Dataset
+
+class Handler(BaseHandler):
+
+    extensions = re.compile(r"^.*\.ionref$", re.IGNORECASE)
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def parse_constraints(self, environ):
+        print ">> Start parse_constraints"
+
+        try:
+            config = ConfigObj(self.filepath, unrepr=True)
+        except:
+            message = "Unable to open file '%s'." % self.filepath
+            raise OpenFileError(message)
+
+#        ds_url = environ["pydap.handlers.test.ds_url"]
+        dsattrs = config.get('dataset', {}).copy()
+        name = dsattrs.pop('name', os.path.split(self.filepath)[1])
+        ds_url = dsattrs.pop('url', None)
+        if ds_url is None:
+            raise OpenFileError("Dataset url not specified")
+
+        if ds_url.startswith("&"):
+            ds_url = os.environ.get("PWD") + ds_url.replace("&","")
+
+        print "Data URL: %s" % ds_url
+
+        ds = Dataset(ds_url)
+
+        gbls={}
+        for k in ds.ncattrs():
+            gbls[k]=ds.getncattr(k)
+
+
+        dataset_type = DatasetType(name=name, attributes={'NC_BLOBAL': gbls})
+
+        fields, queries = environ['pydap.ce']
+        fields = fields or [[(name, ())] for name in ds.variables]
+        print fields
+        print queries
+
+#        for vk in ds.variables:
+        for fvar in fields:
+            while fvar:
+                name, slice_ = fvar.pop(0)
+                if name in ds.variables:
+                    var = ds.variables[name]
+
+                    dtype = str(var.dtype)
+                    if dtype == '|S1':
+                        continue
+
+                    atts={}
+                    for ak in var.ncattrs():
+                        atts[ak] = var.getncattr(ak)
+
+                    dat=var[:]
+                    if isinstance(dat, numpy.ma.core.MaskedArray):
+                        dat = dat.data
+                    elif isinstance(dat, numpy.ndarray):
+                        dat = dat
+
+                    if dtype == '|S1':
+                        dat = numpy.array([''.join(row) for row in numpy.asarray(dat)])
+                        dtype = 'S'
+
+        #            print dat
+
+                    dataset_type[name] = BaseType(name=name, data=dat, shape=var.shape, type=dtype, dimensions=var.dimensions, attributes=atts)
+
+        dataset_type._set_id()
+        dataset_type.close = ds.close
+
+        print ">> End parse_constraints"
+        return dataset_type
+
+if __name__ == '__main__':
+    import sys
+    from paste.httpserver import serve
+
+    application = Handler(sys.argv[1])
+    serve(application, port=8001)
