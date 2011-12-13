c3327c67509f5bb4fdcaa8334398cfe47c3e958a
Christopher Mueller <cmueller@asascience.com>
2011-12-13 14:27:40 -0500

    Added line to ensure IonObjects are available in the pydap.handlers.ion module

--- scripts/paster.py
+++ scripts/paster.py
@@ -4,4 +4,5 @@ import paste.script.command
 
 def start_paster():
 	from gevent import monkey; monkey.patch_all()
+	from pyon.core.bootstrap import bootstrap_pyon; bootstrap_pyon()
 	paste.script.command.run()
